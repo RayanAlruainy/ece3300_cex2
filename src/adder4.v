cce098196e6cb7059670ceb48142a0d525e78a10
RayanAlruainy <116385300+RayanAlruainy@users.noreply.github.com>
2026-09-21 17:42:56 -0700

    Update adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -16,12 +16,12 @@ module adder4(
 wire cout0;
 wire cout1;
 wire cout2;
-wire cout3;
+//wire cout3;
 
 	fulladd fa0(.x(a[0]),.y(b[0]), .cin(1'b0), .sum(result[0]), .cout(cout0));
 	fulladd fa1(.x(a[1]),.y(b[1]), .cin(cout0), .sum(result[1]), .cout(cout1));
 	fulladd fa2(.x(a[2]),.y(b[2]), .cin(cout1), .sum(result[2]), .cout(cout2));
-	fulladd fa3(.x(a[3]),.y(b[3]), .cin(cout2), .sum(result[3]), .cout(cout3));
+	fulladd fa3(.x(a[3]),.y(b[3]), .cin(cout2), .sum(result[3]), .cout(result[4]));
 endmodule
 
 module fulladd(
